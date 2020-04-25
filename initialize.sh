f450cae6f19a28ff05437225df35bc162a1add31
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-04-25 23:57:29 +0000

    Start of init script working.
    Sets up Jenkins, an EKS cluster with an initial deployment
    and loadbalancer, and a DNS record set pointing to the LB.

--- initialize.sh
+++ initialize.sh
@@ -1,26 +1,29 @@
 #!/usr/bin/env bash
 
+alert () {
+  BLUE='\033[0;34m'
+  NC='\033[0m'
+  echo -e "${BLUE}$1${NC}"
+}
+
 # Create JenkinsBox instance
+alert "Creating Jenkins machine..."
 cd cloudformation
 ./create.sh JenkinsBox
 
 # Set up initial EKS cluster
+alert "Setting up initial EKS cluster..."
 cd ../eksctl
 eksctl create cluster -f initial-cluster.yaml
 
 # Apply initial deployment and load balance service
+alert "Setting up initial deployment and load balancer..."
 cd ../kubectl
 kubectl apply -f initial-deployment.yaml
 kubectl apply -f initial-lb-svc.yaml
 
 # Set up initial DNS to the load balancer
-hostname=$(kubectl get svc cyanlb | grep "hostname" | awk '{print $3}')
-while [ hostname == "<pending>"]
-do
-  wait 30
-  hostname=$(kubectl get svc cyanlb | grep "hostname" | awk '{print $3}')
-done
-
+alert "Setting up DNS record set for load balancer..."
 cd ../cloudformation/DNS
 ./dns.py
 
