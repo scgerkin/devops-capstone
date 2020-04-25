147fa8ac263b848e7ad578e1cb737cfe67f90fa8
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-04-25 21:10:50 +0000

    Make script a little more readable, add shebang

--- cloudformation/create.sh
+++ cloudformation/create.sh
@@ -1,5 +1,13 @@
+#!/usr/bin/env bash
+
 stackName=$1
-aws cloudformation create-stack --stack-name $stackName --region=us-east-2 --template-body file://$stackName/stack.yaml --parameters file://.secrets/$stackName.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"
+aws cloudformation create-stack \
+  --stack-name $stackName \
+  --region=us-east-2 \
+  --template-body file://$(pwd)/$stackName/stack.yaml \
+  --parameters file://$(pwd)/.secrets/$stackName.json \
+  --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"
+
 echo "Will wait for stack status 'CREATE_COMPLETE'. No output will be written while waiting."
 echo "Alert will be posted once status is reached."
 echo "This can be safely exited with SIGINT (CTRL+C) without affecting creation."
