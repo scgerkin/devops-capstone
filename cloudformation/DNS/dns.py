201b76338ac65cdf32802ffb80e922c455c6a9a8
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-04-26 20:06:56 +0000

    major refactor of dns script to fix it

--- cloudformation/DNS/dns.py
+++ cloudformation/DNS/dns.py
@@ -3,17 +3,76 @@ import os
 import time
 import sys
 
-kube_svc_name = "cyanlb-blue"
-stack_name = "CyanDnsRecordSet"
-region = "us-east-2"
-template_file = "dnsrecordset.yaml"
-record_name = "cyan"
+
+def get_hostname(kube_svc_name):
+  shell_cmd = """
+    kubectl get svc {0} -o yaml \
+    | grep \"hostname\" \
+    | awk '{{print $3}}'
+  """.format(kube_svc_name)
+  result = os.popen(shell_cmd).read().strip()
+  while not result:
+    print("Hostname is not yet ready, waiting 15 seconds...")
+    time.sleep(15)
+    result = os.popen(shell_cmd).read().strip()
+  print("Hostname: {0}".format(result))
+  return result
 
 
-def set_args():
+def get_lb_name(hostname):
+  result = hostname.split('.')[0].split('-')[0]
+  print("LoadBalancerName: {0}".format(result))
+  return result
+
+
+def get_hosted_zone_id(lb_name):
+  shell_cmd = """
+    aws elb describe-load-balancers \
+      --output=text \
+      --load-balancer-names={0} \
+      --query "LoadBalancerDescriptions[0].CanonicalHostedZoneNameID"
+  """.format(lb_name)
+  result = os.popen(shell_cmd).read().strip()
+  print("CanonicalHostedZoneNameId: {0}".format(result))
+  return result
+
+
+def create_record(stack_name, region, template_file, hostname, hosted_zone_id,
+                  record_name):
+  shell_cmd = """
+    aws cloudformation create-stack \
+      --stack-name {0} \
+      --region={1} \
+      --template-body file://{2} \
+      --parameters \
+        ParameterKey=Target,ParameterValue={3} \
+        ParameterKey=TargetHostedZoneId,ParameterValue={4} \
+        ParameterKey=RecordName,ParameterValue={5}
+  """.format(stack_name, region, template_file, hostname, hosted_zone_id,
+             record_name)
+  result = os.popen(shell_cmd).read().strip()
+  print("Stack ARN: {0}".format(result))
+  return result
+
+
+def await_creation(region, stack_name):
+  shell_cmd = """
+    aws cloudformation wait stack-create-complete \
+      --region={0} \
+      --stack-name {1}
+  """.format(region, stack_name)
+  print("Will await creation of stack: {0}".format(stack_name))
+  os.system(shell_cmd)
+  print("Stack creation is complete.")
+
+
+def main():
   if len(sys.argv) == 1:
-    # use defaults above
-    return
+    kube_svc_name = "cyanlb-blue"
+    stack_name = "CyanDnsRecordSet"
+    region = "us-east-2"
+    template_file = "dnsrecordset.yaml"
+    record_name = "cyan"
   elif len(sys.argv) == 6:
     kube_svc_name = sys.argv[1]
     stack_name = sys.argv[2]
@@ -23,76 +82,14 @@ def set_args():
   else:
     raise Exception("Invalid number of arguments.")
 
-
-def get_hostname() -> str:
-  shell_cmd = f"""
-    kubectl get svc {kube_svc_name} -o yaml \
-    | grep \"hostname\" \
-    | awk '{{print $3}}'
-  """
-  result = os.popen(shell_cmd).read().strip()
-  while not result:
-    print("Hostname is not yet ready, waiting 15 seconds...")
-    time.sleep(15)
-    result = os.popen(shell_cmd).read().strip()
-  print(f"Hostname: {result}")
-  return result
-
-
-def get_lb_name(hostname: str) -> str:
-  result = hostname.split('.')[0].split('-')[0]
-  print(f"LoadBalancerName: {result}")
-  return result
-
-
-def get_hosted_zone_id(lb_name: str) -> str:
-  shell_cmd = f"""
-    aws elb describe-load-balancers \
-      --output=text \
-      --load-balancer-names={lb_name} \
-      --query "LoadBalancerDescriptions[0].CanonicalHostedZoneNameID"
-  """
-  result = os.popen(shell_cmd).read().strip()
-  print(f"CanonicalHostedZoneNameId: {result}")
-  return result
-
-
-def create_record(hostname: str, hosted_zone_id: str) -> str:
-  shell_cmd = f"""
-    aws cloudformation create-stack \
-      --stack-name {stack_name} \
-      --region={region} \
-      --template-body file://{template_file} \
-      --parameters \
-        ParameterKey=Target,ParameterValue={hostname} \
-        ParameterKey=TargetHostedZoneId,ParameterValue={hosted_zone_id} \
-        ParameterKey=RecordName,ParameterValue={record_name}
-  """
-  result = os.popen(shell_cmd).read().strip()
-  print(f"Stack ARN: {result}")
-  return result
-
-
-def await_creation(region: str, stack_name: str):
-  shell_cmd = f"""
-    aws cloudformation wait stack-create-complete \
-      --region={region} \
-      --stack-name {stack_name}
-  """
-  print(f"Will await creation of stack: {stack_name}")
-  os.system(shell_cmd)
-  print("Stack creation is complete.")
-
-
-def main():
-  hostname = get_hostname()
+  hostname = get_hostname(kube_svc_name)
   lb_name = get_lb_name(hostname)
   hosted_zone_id = get_hosted_zone_id(lb_name)
-  stack_arn = create_record(hostname, hosted_zone_id)
+  stack_arn = create_record(stack_name, region, template_file, hostname,
+                            hosted_zone_id, record_name)
   await_creation(region, stack_name)
-  print(f"http://{record_name}.scgrk.com")
+  print("http://{0}.scgrk.com".format(record_name))
 
 
 if __name__ == "__main__":
-  set_args()
   main()
