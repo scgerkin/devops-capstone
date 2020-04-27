edcd7f28dc32910ef42fbbef0dd88a1b1e819bf1
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-04-27 04:24:05 +0000

    Make a breaking change to linting

--- src/main/java/com/scgrk/cyanapp/CyanController.java
+++ src/main/java/com/scgrk/cyanapp/CyanController.java
@@ -8,7 +8,8 @@ import org.springframework.web.servlet.ModelAndView;
 public class CyanController {
 
   @GetMapping("/")
-  public ModelAndView getIndex() {
+  public ModelAndView getIndex()
+  {
     return new ModelAndView("index");
   }
 }
